07125df071fd1d9ac423745ed6d6aaf4b3ea8822
Chun <cchan.xzy@gmail.com>
2019-09-08 21:10:19 +0100

    chore: update read me with links

--- src/example.tsx
+++ src/example.tsx
@@ -4,12 +4,6 @@ import ReactDOM from 'react-dom';
 import Example1 from './examples/Example1';
 import Example2 from './examples/Example2';
 
-ReactDOM.render(
-  <Example1 />,
-  document.getElementById('example-1'),
-);
+ReactDOM.render(<Example1 />, document.getElementById('example-1'));
 
-ReactDOM.render(
-  <Example2 />,
-  document.getElementById('example-2'),
-);
+ReactDOM.render(<Example2 />, document.getElementById('example-2'));
