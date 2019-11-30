392082e7f59e202c97345d7e8d2a552adebced16
Chun <11034578+cchanxzy@users.noreply.github.com>
2019-11-30 21:53:22 +0000

    fix: modify typescript config to correctly output types (#19)

--- dist/components/utilities.d.ts
+++
@@ -1,3 +0,0 @@
-export declare const addCommas: (value: number) => string;
-export declare const removeCommas: (value: string) => string;
-export declare const checkIsValidNumber: (input: number, limit: number) => boolean;
