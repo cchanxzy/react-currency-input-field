89a2d59fa8b361bd8d7692de95af8a5bb3b6884d
Chun <11034578+cchanxzy@users.noreply.github.com>
2021-03-24 21:06:14 +0000

    chore(deps): upgrade many dependencies with breaking versions (#151)

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,7 +2,6 @@ module.exports = {
   parser: '@typescript-eslint/parser', // Specifies the ESLint parser
   extends: [
     'plugin:@typescript-eslint/recommended',
-    'prettier/@typescript-eslint',
     'plugin:prettier/recommended',
     'plugin:react/recommended',
   ],
