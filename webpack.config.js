af1c9c4918ff3f9912ef99dbdaf5432ea5db6035
George Smith <georges8899@gmail.com>
2021-07-08 19:15:41 +0100

    perf: add tree shaking support (#182)

--- webpack.config.js
+++
@@ -1,6 +0,0 @@
-/* eslint-disable @typescript-eslint/no-var-requires */
-
-const prodConfig = require('./webpack/webpack-prod.config');
-const devConfig = require('./webpack/webpack-dev.config');
-
-module.exports = [prodConfig, devConfig];
