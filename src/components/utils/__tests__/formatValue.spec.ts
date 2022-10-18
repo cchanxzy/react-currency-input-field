36ad0edea389d88fe11146e00fdfd999e4c0b19c
Max <32452032+max-sym@users.noreply.github.com>
2022-10-18 14:03:51 +0100

    fix: no decimals if defined decimalScale, input value as  X.00 and intlConfig, but without no currency (#262)
    * fix: no decimals if has decimalScale but no currency in intlConfig
    
    * fix: previous fix, add de-DE locale test
    
    * fix: add more specific test description for prev fix

--- src/components/utils/__tests__/formatValue.spec.ts
+++ src/components/utils/__tests__/formatValue.spec.ts
@@ -516,5 +516,25 @@ describe('formatValue', () => {
         })
       ).toEqual(`£123.98$`);
     });
+
+    it('should add decimals if intlConfig has no currency provided, decimalScale defined and the input value is X.00', () => {
+      expect(
+        formatValue({
+          value: '123.00',
+          intlConfig: { locale: 'en-US' },
+          decimalScale: 2,
+        })
+      ).toEqual(`123.00`);
+    });
+
+    it('should add decimals if intlConfig has no currency provided, decimalScale defined and the input value is X.00 (de-DE)', () => {
+      expect(
+        formatValue({
+          value: '123.00',
+          intlConfig: { locale: 'de-DE' },
+          decimalScale: 2,
+        })
+      ).toEqual(`123,00`);
+    });
   });
 });
