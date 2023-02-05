49c8b05cf8bb44098372113f37f88ab451ee2285
Bartlomiej Kochanowicz <bartlomiej.kochanowicz@landingi.com>
2023-02-05 03:05:24 +0100

    add regexp

--- src/components/utils/removeInvalidChars.ts
+++ src/components/utils/removeInvalidChars.ts
@@ -1,10 +1,33 @@
 import { escapeRegExp } from './escapeRegExp';
 
+const countryCodesWithAbbreviationsChars = [
+  // with K
+  /[A-Z][A-Z]K/,
+  /[A-Z]K[A-Z]/,
+  /K[A-Z][A-Z]/,
+  // with M
+  /[A-Z][A-Z]M/,
+  /[A-Z]M[A-Z]/,
+  /M[A-Z][A-Z]/,
+  // with B
+  /[A-Z][A-Z]B/,
+  /[A-Z]B[A-Z]/,
+  /B[A-Z][A-Z]/,
+];
+
 /**
  * Remove invalid characters
  */
 export const removeInvalidChars = (value: string, validChars: ReadonlyArray<string>): string => {
   const chars = escapeRegExp(validChars.join(''));
+
   const reg = new RegExp(`[^\\d${chars}]`, 'gi');
-  return value.replace(reg, '');
+
+  let clearValue = value;
+
+  countryCodesWithAbbreviationsChars.forEach((regExp) => {
+    clearValue = clearValue.replace(regExp, '');
+  });
+
+  return clearValue.replace(reg, '');
 };
