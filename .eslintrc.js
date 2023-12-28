0c13902641a1424eec8086ed34c34307d1abca7c
Chun <cchanxzy@gmail.com>
2023-12-28 12:35:30 +0000

    chore: update eslint and prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,36 @@
 module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    es2021: true,
+    jest: true,
+  },
   parser: '@typescript-eslint/parser', // Specifies the ESLint parser
-  extends: [
-    'plugin:@typescript-eslint/recommended',
-    'plugin:prettier/recommended',
-    'plugin:react/recommended',
-  ],
   parserOptions: {
-    ecmaVersion: 2018,
+    ecmaVersion: 'latest',
     sourceType: 'module',
     ecmaFeatures: {
       jsx: true,
     },
   },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'prettier',
+  ],
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: ['commitlint.config.js', '.eslintrc.js'],
+      parserOptions: {
+        sourceType: 'script',
+      },
+    },
+  ],
   rules: {
     // Place to specify ESLint rules. Can be used to overwrite rules specified from the extended configs
   },
@@ -20,7 +39,5 @@ module.exports = {
       version: 'detect', // Tells eslint-plugin-react to automatically detect the version of React to use
     },
   },
-  env: {
-    jest: true,
-  },
+  ignorePatterns: ['dist'],
 };
--- src/components/utils/escapeRegExp.ts
+++ src/components/utils/escapeRegExp.ts
@@ -4,5 +4,5 @@
  * See: https://stackoverflow.com/questions/17885855/use-dynamic-variable-string-as-regex-pattern-in-javascript
  */
 export const escapeRegExp = (stringToGoIntoTheRegex: string): string => {
-  return stringToGoIntoTheRegex.replace(/[-\/\\^$*+?.()|[\]{}]/g, '\\$&');
+  return stringToGoIntoTheRegex.replace(/[-/\\^$*+?.()|[\]{}]/g, '\\$&');
 };
