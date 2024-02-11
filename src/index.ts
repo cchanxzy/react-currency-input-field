026329f30aa23cebbb90ddeadce62be94324567c
Marco Romagnuolo <marcoromag@gmail.com>
2024-02-11 10:34:59 +0100

    feat: export utility function 'cleanValue' (#334)

--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,4 @@ import CurrencyInput from './components/CurrencyInput';
 export { CurrencyInputProps, CurrencyInputOnChangeValues } from './components/CurrencyInputProps';
 export default CurrencyInput;
 export { formatValue } from './components/utils/formatValue';
+export { cleanValue } from './components/utils/cleanValue';
