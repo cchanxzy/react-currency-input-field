121c3a8edbf601fefd49d5f206a0f63880026c6f
Rizki Maulana Citra <rmaulana.citra@gmail.com>
2024-11-09 21:46:49 +0700

    feat: export `IntlConfig` props

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import CurrencyInput from './components/CurrencyInput';
 
-export { CurrencyInputProps, CurrencyInputOnChangeValues } from './components/CurrencyInputProps';
+export { CurrencyInputProps, CurrencyInputOnChangeValues, IntlConfig } from './components/CurrencyInputProps';
 export default CurrencyInput;
 export { formatValue } from './components/utils/formatValue';
 export { cleanValue } from './components/utils/cleanValue';
